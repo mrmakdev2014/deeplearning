86dabcce91c41b6193bcc62dbcb7d5af454e65a2
Mohamed Makeen <MrMakDev@outlook.com>
2019-09-14 15:26:19 +0300

    Support vector Regression

---
+++ 002_Video1_Supervised_Regression.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 14 15:03:48 2019
+
+@author: Makeen
+"""
+"""
+Video Link 
+https://youtu.be/7sJ9hQYpkbU?list=PLYW0LRZ3ePo4TFeouzYff88TStgS-X52R&t=1134
+"""
+import numpy as np
+import matplotlib.pyplot as plt 
+from sklearn import svm 
+from matplotlib import style 
+style.use("ggplot")
+
+
+my_input  = np.array([[3,2],[6,6],
+         [2.6,3],[7,8] ,[3.5,5],
+         [6,11]
+         ]
+        )
+
+my_output = [0,1,0,1,0,1]
+
+""" svm.SVC  = Support Vector Classfication  """
+my_model = svm.SVC(kernel='linear' ,C=1.0)
+my_model.fit(my_input , my_output)
+
+print ("SVC predicate [0.5,0.8] : " , my_model.predict([[0.5,0.8]]))
+print ("SVC predicate [8.5,10] : " , my_model.predict([[8.5,10]]))
+
+
+plt.scatter(my_input[:,0] , my_input[:,1],c=my_output)
+
+
+""" c= r mean color = red """
+plt.scatter(0.5,0.8,c='r')
+plt.scatter(8.5,10,c='r')
+
+plt.show()
+
+
+
+
